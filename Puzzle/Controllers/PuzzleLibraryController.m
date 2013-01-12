4df84c30a3cdf253dd493444c7a5fafed74b37cc
Pulkit Kathuria <kevincobain2000@gmail.com>
2013-01-12 14:48:56 +0900

    Label Tap Image Added

--- Puzzle/Controllers/PuzzleLibraryController.m
+++ Puzzle/Controllers/PuzzleLibraryController.m
@@ -206,7 +206,7 @@
             float w = self.view.bounds.size.width;
             cell = [[PhotoCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
             cell.photo = [[UIImageView alloc] initWithFrame:CGRectMake((w-IMAGE_SIZE)/2, (w-IMAGE_SIZE)/2, IMAGE_SIZE, IMAGE_SIZE)];
-            cell.photo.layer.cornerRadius = 10;
+            cell.photo.layer.cornerRadius = 5;
             cell.photo.layer.masksToBounds = YES;
             [cell addSubview:cell.photo];
             UIView *v = [[UIView alloc] init];
